08a4a820694b941041cce9244ca2598d246bcb92
irena_weigand <irena.weigand@gmail.com>
2023-09-22 18:12:32 +0200

    zadatak22/9

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import noviKorisnik from "./noviKorisnik"
 
 function App() {
   const [tekst, setText] = useState("")
@@ -19,10 +20,10 @@ function App() {
   return (
   <form onSubmit={handleSubmitForm}>
     <label>
-        Upiši tekst:
+        Upiši tekst: <br />
         <input type='tekst' name="tekst" value={tekst} onChange={handleTextChange}/>
     </label>
-    
+    <p></p>
     <div>
       <input type="submit" value="Početno stanje" />
     </div>
